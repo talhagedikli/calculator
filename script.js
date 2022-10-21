f2135af2d67f78db6ea2fa92634059586dac8fed
talhagedikli <talhagedikli@protonmail.com>
2022-10-21 13:22:28 +0300

    added basic functionality to buttons

--- script.js
+++ script.js
@@ -1,12 +1,53 @@
+const calculator = document.querySelector('.calculator');
+const resultScreen = calculator.querySelector('.result-screen');
+const buttons = calculator.querySelector('.buttons');
+const numbers = buttons.querySelectorAll('.number');
+const operators = buttons.querySelectorAll('.operator');
+
+let numberArray = ['', '']
+let activeNumber = 0;
+let result = '';
+let calculating = true;
 
 
+numbers.forEach((e) => {
+    e.addEventListener('click', e => {
+        calculating = true;
+        numberArray[activeNumber] += e.target.innerText.toString();
+        updateResultScreen();   
+    })
+})
 
+operators.forEach(e => {
+    e.addEventListener('click', e => {
+        activeNumber++;
+        if (activeNumber >= 2) {
+            calculating = false;
+            result = add(numberArray[0], numberArray[1]);
+            updateResultScreen();
+            resetCalculator();
+        }
 
+    })
+})
 
+function updateResultScreen() {
+    resultScreen.textContent = activeNumber < 2 ? numberArray[activeNumber] : result;
+}
+
+function resetCalculator() {
+    numberArray = ['', ''];
+    activeNumber = 0;
+    result = '';
+}
+
+function getCurrentNum() {
+    return numberArray[activeNumber];
+}
 
 // Functions
 function add(a, b) {
-    return a + b;
+    return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -24,3 +65,9 @@ function divide(a, b) {
 function operate(func, a, b) {
     return func(a, b);
 }
+
+function main() {
+    updateResultScreen();
+}
+
+main();
