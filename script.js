9c2412e2b0b46b7fdc4f656dfcc8113b95ed4d15
talhagedikli <talhagedikli@protonmail.com>
2022-10-21 11:02:57 +0300

    added basic functions

---
+++ script.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+// Functions
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply() {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
